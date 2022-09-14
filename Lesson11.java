e6eeb2390bf694f591480e533a68651754d9d56e
Alex-Krasko <92627140+Alex-Krasko@users.noreply.github.com>
2022-09-14 20:31:09 +0300

    FloatNum_Exceptions

---
+++ Lesson11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Lesson11 {
+    public static void main(String[] args) {
+        System.out.println("Введено: " + GetNum());
+    }
+
+    public static float GetNum(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        float num = 0;
+        try {
+            num = sc.nextFloat();
+        } catch (Exception e){
+            System.out.println("Некорректный ввод. Повторите попытку.");
+            num = GetNum();
+        } 
+        return num;
+    }
+}
